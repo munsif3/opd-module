e256edb1d27c4de604a46b9fc026bcaff692ea3c
Munsif Musthafa <immsf5@gmail.com>
2017-06-28 01:50:28 +0530

    First Commit

---
+++ server.js
@@ -0,0 +1,28 @@
+'use strict';
+
+// Adding Dependencies
+const express = require('express');
+
+// Creating the Express Application
+const app = express();
+
+// Setting the PORT
+const port = process.env.PORT || 3030;
+
+// Enabling file usage for Frontend
+app.use('/app', express.static(__dirname + '/public'));
+app.use('/app/modules', express.static(__dirname + '/bower_components'));
+
+// Returning for Root Request
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+// Create Server and Listen
+app.listen(port, err => {
+    if (err) {
+        console.log('Error listening on port ' + port);
+        return;
+    }
+    console.log('Keep an ear on ' + port);
+});
