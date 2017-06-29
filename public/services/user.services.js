5741d6f767c8889c6aa03593c1e1089265bd5e1e
Munsif Musthafa <immsf5@gmail.com>
2017-06-29 13:55:07 +0530

    modified the index file

--- public/services/user.services.js
+++ public/services/user.services.js
@@ -4,6 +4,9 @@ var userApp = angular.module('UserApp');
 
 userApp.factory('UserService', ['$http',
     function () {
-
+        return{
+            get: () => $http.get('http://localhost:8081/theatres').then(response => response.data)
+        }
     }
+
 ]);
