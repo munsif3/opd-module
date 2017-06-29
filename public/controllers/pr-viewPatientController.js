10da57e17610fce0991820492936dfe185c42018
KiruthigaBalendran <kiruthiga03@gmail.com>
2017-06-29 22:25:35 +0530

    console added to test

--- public/controllers/pr-viewPatientController.js
+++ public/controllers/pr-viewPatientController.js
@@ -1,7 +1,9 @@
 angular.module('viewPatientController',['viewPatientService'])
 .controller('viewPatientCtrl',['$scope', '$location', 'viewPatientService',
     function ($scope, $location, patientService){
+    	console.log("viewPatientController Activated");
         function getPatients() {
+        	console.log("getPatients method initiated");
             patientService.getPatientDetails().then(patients => {
                 $scope.patients = patients;
             });
